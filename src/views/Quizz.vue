18b65c40ac2e8f164de51c6e1b67f077b026d754
QuentinRo <quentin.rossier@cpnv.ch>
2019-02-03 22:35:01 +0100

    Do the quizz stuff

--- src/views/Quizz.vue
+++ src/views/Quizz.vue
@@ -1,37 +1,67 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
 
-        <div v-for="(quiz, index) of items.quizzs"  style="display:flex" :key="quiz.id">
-            <div v-if="quiz.id == $route.params.id" style="text-align:center">
-                {{quiz.title}}
-                {{quiz.id}}<br>
-                {{quiz.questions[1].question}}
-                {{quiz.questions[1].answers[1].name}}
+        <div class="row">
+            <div class="col s12 m4 l2"></div>
+            <div class="col s12 m4 l8">
+                <div class="quizz" v-for="(item, index) in this.items.quizzs[this.quizindex].questions">
+                <ul class="collection with-header" >
+                    <li class="collection-header"  style="text-align:center">
+                    {{item.question}}
+                    {{WichQuizz(1)}}
+                    </li>
+                        <div v-for="(answer, index) in item.answers">
+                        <li> <a href="#!" class="collection-item" onclick="this.classList.toggle('active');" v-on:click="nextquestion(index)">{{answer.name}}</a></li>
+                        </div>
+                </ul>
+
+            </div>
             </div>
         </div>
 
-
-        <div class="row col-lg-12 center question" id="header">
-            {{items.quizzs.questions}}
+        <div class="questions" v-show="end">
+            vous avez {{this.correct}} reponse juste
         </div>
-
-
-
     </div>
+
+
 </template>
 
 <script>
     import json from '../quizz.json'
-    export default {
-        name: 'doQuizz',
-        props: {
-            msg: String
+
+export default {
+    name: 'doQuizz',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            items: json,
+            question: null,
+            quizindex: 0,
+            currentQuestion: 0,
+            correct: 0,
+            end: false
+        }
+    }, methods: {
+        WichQuizz: function(index){
+            this.quizindex = index;
+            console.log(this.quizindex);
         },
-        data(){
-            return{
-                items:json
+        //Method to go on the next question
+        nextquestion: function(index){
+            //Scoring
+            if(this.items.quizzs[this.quizindex].questions[this.currentQuestion].answers[index].value == 'true'){
+                this.correct++
+            }
+            //Check if it's the last question or not
+            if(this.currentQuestion == this.items.quizzs[this.quizindex].questions.length - 1) {
+                this.end = true;
+            } else {
+                this.currentQuestion++
             }
         }
     }
+}
 </script>
