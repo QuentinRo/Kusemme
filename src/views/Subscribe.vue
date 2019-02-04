87ffdb6b8f2a5a1144f7c079fbb23d92a1da1bee
QuentinRo <quentin.rossier@cpnv.ch>
2019-02-04 19:38:47 +0100

    new view

--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -2,7 +2,20 @@
     <div class="home">
 
 
-        <h1>:C</h1>
+        <div class="row">
+            <div class="col s12"><p></p></div>
+            <div class="col s12 m6 l4"></div>
+            <div class="col s12 m6 l4"><h4>Créer un compte</h4>
+                <input type="text" class="validate" placeholder="Nom">
+                <input type="text" class="validate" placeholder="Prénom">
+                <input type="text" class="validate" placeholder="Username">
+                <input type="text" class="validate" placeholder="Email">
+                <input type="text" class="form-control" placeholder="Mot de passe">
+                <input type="text" class="form-control" placeholder="Confirmer mot de passe">
+                <button class="btn waves-effect waves-light" type="submit" name="action">S'inscrire</button>
+            </div>
+
+        </div>
     </div>
 </template>
 
