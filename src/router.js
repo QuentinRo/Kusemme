447ea70ba5698def83babfc8b604b1fdbc7fbec1
ROSSIER Quentin <Quentin.ROSSIER@cpnv.ch>
2019-02-04 15:42:14 +0100

    adding some routes

--- src/router.js
+++ src/router.js
@@ -1,29 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Quizz from './views/Quizz.vue'
+import Login from './views/Login.vue'
+import listQuiz from './components/HelloWorld'
+import Subscribe from './views/Subscribe'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'listQuiz',
+      component: listQuiz
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Login
+
     },
     {
         path: '/quizz/:id',
         name: 'quizz',
         component: Quizz
-    }
+    },
+      {
+          path: '/Subscribe',
+          name: 'subscribe',
+          component: Subscribe
+      }
   ]
 })
