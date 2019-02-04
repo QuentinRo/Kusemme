aa4056eddf0824a11ce2454fc4f5bf679bbf0de0
QuentinRo <quentin.rossier@cpnv.ch>
2019-02-04 19:36:35 +0100

    add login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="home">
 
-
-        <h1>:)</h1>
+        <div class="row">
+            <div class="col s12"><p></p></div>
+            <div class="col s12 m6 l4"></div>
+            <div class="col s12 m6 l4"><h4>Login</h4>
+                <input type="text" class="form-control" placeholder="Username">
+                <input type="text" class="form-control" placeholder="°°°°°°°°°"><a class="waves-effect waves-light btn">Se connecter</a>
+                <a href="register.html">Pas encore de compte ? Cliquer ici pour s'inscrire !</a>
+            </div>
+    </div>
     </div>
 </template>
 
